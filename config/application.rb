6cc7452380c5da1526c8c8b76cebaef833783215
kohei <soo.kohei@gmail.com>
2020-07-08 00:41:35 +0900

    キャッシュ削除

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Favorock
+module Otofile
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -19,5 +19,7 @@ module Favorock
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.assets.initialize_on_precompile = false
   end
 end
