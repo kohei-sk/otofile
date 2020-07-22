2d1053978827b260ebe49896d6d5cf5c55878c39
kohei <soo.kohei@gmail.com>
2020-07-22 09:16:32 +0900

    公開前調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -915,9 +915,6 @@ $(document).ready(function () {
             var bar = counter.children('span');
             var percent = counter.attr('id').split('_')[1];
 
-            // setTimeout(function () {
-            //     bar.css('width', `${percent}%`);
-            // }, 800);
             setTimeout(function () {
                 bar.animate({
                     "width": `${percent}%`
